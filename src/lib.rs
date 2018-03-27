4793948882525fc10fc4bff24a5eb6c5fe558235
Mitsuharu Seki <mitsu1986@gmail.com>
2018-03-28 08:22:37 +0900

    Fix i128_type feature fot stable docs

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@
 
 #![cfg_attr(feature = "docs", feature(staged_api))]
 #![cfg_attr(feature = "docs", stable(feature = "default", since = "0.1.0"))]
+#![cfg_attr(feature = "docs", feature(i128_type))]
 
 #[cfg(feature = "num-complex")]
 extern crate num_complex;
